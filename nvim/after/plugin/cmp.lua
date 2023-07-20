954ca475d1cb7511ca2e4d0921a64d20b9a80f0e
Elias Rojas <rojas.elias@outlook.com>
2023-07-20 18:36:34 -0300

    epa

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -8,6 +8,7 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
@@ -19,38 +20,19 @@ cmp.setup({
 		end,
 	},
 	window = {
-		--completion = cmp.config.window.bordered(),
-		--documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 		["<C-Space>"] = cmp.mapping.complete(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-			-- they way you will only jump inside the snippet region
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+        ["Tab"] = cmp.config.disable,
+        ["S-Tab"] = cmp.config.disable
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
--- nvim/lua/elo/lazy.lua
+++ nvim/lua/elo/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
 	"saadparwaiz1/cmp_luasnip",
 	"hrsh7th/nvim-cmp",
 	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
 	"hrsh7th/cmp-nvim-lsp",
 	"L3MON4D3/LuaSnip",
 	"rafamadriz/friendly-snippets",
